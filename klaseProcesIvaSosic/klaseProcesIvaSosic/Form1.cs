7f7ef5a7ad6d97796e6b7af6e22e7a84c9a4c857
a <145933200+ivasosic@users.noreply.github.com>
2023-11-05 19:14:00 +0100

    Add files via upload

---
+++ klaseProcesIvaSosic/klaseProcesIvaSosic/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace klaseProcesIvaSosic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start("IExplore.exe");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start("WINWORD.exe");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process.Start("Excel.exe");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo("msedge.exe")
+            {
+                WindowStyle = ProcessWindowStyle.Normal,
+                Arguments = "www.sser.hr"
+            };
+            Process.Start(startInfo);
+        }
+    }
+}
+    
